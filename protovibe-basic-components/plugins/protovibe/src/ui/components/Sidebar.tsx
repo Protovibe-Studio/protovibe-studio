a02b39d775f72575c16462fd168617074196bdff
Protovibe Admin <protovibe.studio@gmail.com>
2026-04-02 22:25:49 +0200

    Undo not working scrolling fix for sidebar

--- protovibe-basic-components/plugins/protovibe/src/ui/components/Sidebar.tsx
+++ protovibe-basic-components/plugins/protovibe/src/ui/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 // plugins/protovibe/src/ui/components/Sidebar.tsx
-import React, { useLayoutEffect, useRef } from 'react';
+import React from 'react';
 import { useProtovibe } from '../context/ProtovibeContext';
 import { Header } from './Header';
 import { Tabs } from './Tabs';
@@ -19,21 +19,6 @@ type SidebarProps = {
 
 export const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
   const { currentBaseTarget, activeData, isMutationLocked } = useProtovibe();
-
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-  const savedScrollRef = useRef(0);
-  const prevActiveRef = useRef(activeData);
-
-  if (prevActiveRef.current !== activeData) {
-    savedScrollRef.current = scrollContainerRef.current?.scrollTop ?? 0;
-    prevActiveRef.current = activeData;
-  }
-
-  useLayoutEffect(() => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollTop = savedScrollRef.current;
-    }
-  }, [activeData]);
 
   const stopScrollEventEscape = (event: React.UIEvent<HTMLDivElement>) => {
     event.stopPropagation();
@@ -93,7 +78,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
         onTouchMoveCapture={stopScrollEventEscape}
       >
         <Header />
-        <div ref={scrollContainerRef} style={{ display: 'flex', flexDirection: 'column', flex: 1, overflowY: 'auto' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', flex: 1, overflowY: 'auto' }}>
           <Tabs />
           <BlockEditor />
           {activeData && (
